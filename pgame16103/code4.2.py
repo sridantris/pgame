5668d2b98937abc1f388595647434cb97a082d0b
sridantris <55886889+sridantris@users.noreply.github.com>
2019-11-21 00:08:16 +0900

    praktikum 4

---
+++ pgame16103/code4.2.py
@@ -0,0 +1,26 @@
+myvar="Informatika UMMU"
+print(myvar)
+
+# cetak colom 0 (pertama) dari variabel myvar
+print(myvar[0])
+
+# cetak colom 0 dengan keterangan
+print("myvar [0] : ", myvar[0])
+
+# cetak colom awal sampai colom ke 4
+print(myvar[:3])
+
+# cetak colom awal sampai colom ke 4 dengan keterangan
+print("myvar [:3] : ", myvar[:3])
+
+# cetak semua kecuali colom 0 (pertama)
+print(myvar[1:])
+
+# cetak semua kecuali colom 0 (pertama) dengan keterangan
+print("myvar [1:] : ", myvar[1:])
+
+# cetak semua kecuali colom terakhir
+print(myvar[:-1])
+
+# cetak semua kecuali colom terakhir dengan keterangan
+print("myvar [:-1] : ", myvar[:-1])
