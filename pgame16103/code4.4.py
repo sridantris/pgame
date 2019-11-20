79a18e8d13bdc8ca397533eed821b2258aa9dbb4
sridantris <55886889+sridantris@users.noreply.github.com>
2019-11-21 00:10:07 +0900

    praktikum 4

---
+++ pgame16103/code4.4.py
@@ -0,0 +1,34 @@
+# Buat variabel dan memberikan elemen
+buah=["Durian","Mangga","Rambutan","Anggur","Pepaya"]
+
+# cetak list buah
+print(buah)
+
+# cetak elemen pertama list (elemen pertama adalah 0)
+print("Buah [0] ", buah[0])
+
+# cetak elemen kedua list (elemen kedua adalah 1)
+print("Buah [1] ", buah[1])
+
+# cetak elemen ketiga list sampai akhir elemen
+print("Buah [2:] ", buah[2:])
+
+# cetak elemen terakhir list ( [-1] adalah elemen terakhir)
+print("Buah [-1] ", buah[-1])
+Print("*"*35)
+
+# Buat variabel list mylist dan memberikan elemen
+mylist=["Ternate", 1000, 2000, "Tidore", "Maitara", 3000]
+
+# cetak list mylist
+print("mylist : ", mylist)
+
+# cetak list elemen pertama sampai elemen ke 3
+print("mylist [:3] : ", mylist[:3])
+
+# cetak list elemen ke 2 dan ke 3
+print("mylist[1] : ", mylist[1])
+print("mylist[2] : ", mylist[2])
+
+# operasi antar elemen list : elemen ke 2 + elemen ke 3
+print("mylist[1] + mylist[2] : ", mylist[1] + mylist[2])
