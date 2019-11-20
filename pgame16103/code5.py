6344a07e261d70d23a00d25a3a9113faa42e1d1f
sridantris <55886889+sridantris@users.noreply.github.com>
2019-11-21 00:02:41 +0900

    praktikum_3

---
+++ pgame16103/code5.py
@@ -0,0 +1,35 @@
+#latihan_1
+True and True
+True and False
+True or False
+False or True
+False or False
+True or True
+not True
+not False
+
+#latihan_2
+a = 5
+a += 5
+print(a)
+
+#latihan_3
+a = [1,3,5]
+3 in a
+3 not in a
+b = ['A','B','C','F']
+'A' in b
+
+#latihan_4
+a,b = 20,20
+a is b
+a is not b
+aku,kamu="budi","iwan"
+aku is kamu
+aku is "budi"
+kamu is "iwan"
+
+#latihan_5
+a,b = 33,44
+a & b
+a | b
