05e877282ef70cb941f0cc97543acf254f7f1c66
sridantris <55886889+sridantris@users.noreply.github.com>
2019-11-20 23:55:22 +0900

    praktikum1.4

---
+++ pgame16103/code4.py
@@ -0,0 +1,24 @@
+# Dibawah ini adalah latihan 5
+
+print (4+2)
+print (4-2)
+print (4*2)
+print (4/2)
+print (4%3)
+print (4**2)
+print (4//3)
+
+# Dibawah ini adalah latihan 6
+
+print (4 == 4)
+print (4 == 5)
+print (4 != 4)
+print (4 != 5)
+print (5 > 4)
+print (4 > 5)
+print (5 < 4)
+print (4 < 5)
+print (4 >= 4)
+print (4 >= 5)
+print (4 <= 4)
+print (4 <= 3)
