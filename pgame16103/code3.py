a840b873aa59cc069495501411e3065dbd166f0d
sridantris <55886889+sridantris@users.noreply.github.com>
2019-11-20 23:53:43 +0900

    praktikum1.3

---
+++ pgame16103/code3.py
@@ -0,0 +1,12 @@
+# Ini Tugas 3. Praktik Git untuk MK Pemrograman Game
+# juga untuk latihan git push dan pull from terminal
+
+# Latihan Buat Variabel : 
+nama = "Nama Saya 	: Sri Dantris" 
+print (nama)
+npm = "NPM 		: 121055520116103" 
+print (npm)
+semester = "Semester 	: 7 (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
