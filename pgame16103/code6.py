112a0711658ae0c949f10d5a11fc4a4ef6f10758
sridantris <55886889+sridantris@users.noreply.github.com>
2019-11-21 00:05:53 +0900

    praktikum_4

---
+++ pgame16103/code6.py
@@ -0,0 +1,82 @@
+# BOOLEAN
+myvar=True
+print(myvar)
+
+# Complex
+myvar =2+4j
+print(myvar)
+
+# Long
+myvar=999999999999
+print(myvar)
+
+# None
+myvar = None
+print(myvar)
+
+# Hexadecimal
+myvar=0xbb
+print(myvar)
+
+# Float
+myvar = 20.43
+print(myvar)
+
+print(myvar + 5.6)
+
+# Integer
+myvar = 22
+print(myvar+8)
+
+# String
+myvar = "ini string"
+print(myvar)
+
+print(myvar[0])
+print(myvar[1])
+print(myvar[-1])
+print(myvar[1:])
+print(myvar[:1])
+
+# List
+myvar=[100, 200, 300, 400]
+print(myvar)
+print(myvar[0])
+print(myvar[1])
+print(myvar[3])
+
+myvar=["Satu","Dua", "Tiga", "Empat"]
+print(myvar)
+print(myvar[0])
+print(myvar[1])
+print(myvar[3])
+
+myvar=[1, "Dua", 3, "Empat"]
+print(myvar)
+print(myvar[0])
+print(myvar[1])
+
+# buat variabel dan beri nilai
+a = 4
+b = 5
+c = 6.3
+
+# cetak variabel ke layar
+print(a)
+print(b)
+print(c)
+
+# cetak '*' sebanyak 35 kali
+print('*'*35)
+
+# cetak variabel dengan keterangan ke layar
+print("Variable A :", a )
+print("Variable B : %d " % b )
+print("Variable C : %f " % c )
+print('*'*35)
+
+# cetak variabel dengan keterangan ke laya
+# operasi + , * , / dan cetak ke layar
+print("Variable A+B : ", a+b )
+print("Variable A*B : ", a*b )
+print("Variable C/3 : ", c/3 )
