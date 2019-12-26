e44d6244997dc0a73bba5c5eb5fa1956e4141968
sridantris <55886889+sridantris@users.noreply.github.com>
2019-12-26 23:26:06 +0900

    Praktikum 8

---
+++ pgame16103/Praktikum8.1.py
@@ -0,0 +1,31 @@
+#Operator Penugasan
+
+b = 10 	# Buat variabel b dan isi dengan nilai 10
+print (b)	
+
+a += 5	# Tambahkan nilai 5 ke variabel a
+print (a)
+
+a -= 10
+print (a)
+
+a *= 20
+print (a)
+
+a /= 60
+print (a)
+
+a %= 40
+print (a)
+
+a **= 20
+print (a)
+
+b //= 30
+print (b) 
+a = 5 # Buat variabel a dan isi dengan nilai 5
+a += 5 # Tambahkan nilai 5 ke variabel a
+print(a) # Cetak nilai a 
+10
+# Lanjutkan Latihan untuk operator selanjutnya
+# Sebagaimana Terlihat pada tabel 4
