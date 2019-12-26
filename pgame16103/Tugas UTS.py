dc1a62fc4dd18b64dd34bd258542bd1bcf722b07
sridantris <55886889+sridantris@users.noreply.github.com>
2019-12-26 23:11:50 +0900

    UTS Seminar

--- pgame16103/Tugas UTS.py
+++ pgame16103/Tugas UTS.py
@@ -1,78 +1,95 @@
-Dirangkum Oleh:
-NPM		: 121055520116103
-Nama		: Sri Dantris Anhal
+##Dirangkum Oleh:
+###NPM		      : 121055520116103
+###Nama	      	: Sri Dantris Anhal
+***
 
-Materi Seminar I,
-Waktu		: 10/12/2019 : 09.00
-Tempat		: Rektorat UMMU
-Durasi		: 10.11-11.13 Menit, Sesi Ke-2
-Nama Pemateri	: Dr.Ing. Ridho Rahamadi, Msc.
-Judul		: Data Science
-Isi Materi	: Data Sciance
+##Materi Seminar I,
+###Waktu		      : 10/12/2019 : 09.00
+###Tempat		      : Rektorat UMMU
+###Durasi		      : 10.11-11.13 Menit, Sesi Ke-2
+###Nama Pemateri	: Dr.Ing. Ridho Rahamadi, Msc.
+###Judul		      : Data Science
+###Isi Materi   	: Data Sciance
+***
 a.	Data
-Data adalah Sesuatu yang kita kumpulkan atau Observasi, apa yang kita lihat kita catat contoh kita lihat spit yang lalu lintas ke tidore-ternate perhari kita catat.
+    Data adalah Sesuatu yang kita kumpulkan atau Observasi, apa yang kita lihat kita catat contoh kita lihat spit yang lalu lintas \
+    ke tidore-ternate perhari kita catat.
 b.	Tranformasi Digital
-•	Redefining Data
--	49K photos in Instagram 
--	3.9M Google searches 
--	4.3M Youtube watch 
--	473K Twitter tweets 
--	12.9M text sent 
--	750K Spotify stream 
--	156M emails sent 
--	154K Skype calls
-Refending  Data di atas adalah data yang di akumulasi setiap menit, jadi dari sinilah istilah Big Data muncul dan di sebut dengan Era Digital Transform.
+    â€¢	Redefining Data
+      -	49K photos in Instagram 
+      -	3.9M Google searches 
+      -	4.3M Youtube watch 
+      -	473K Twitter tweets 
+      -	12.9M text sent 
+      -	750K Spotify stream 
+      -	156M emails sent 
+      -	154K Skype calls
+    Refending  Data di atas adalah data yang di akumulasi setiap menit, jadi dari sinilah istilah Big Data muncul dan di sebut dengan Era 
+    Digital Transform.
 c.	Sciance 
-Dari penjelasan di atas science adalah mencari sesautu value – value atau nilai nilai tersebut yang bermanfaat.
-Konsep Data science ini adalah metode lama tapi baru, maksudnya adalah metode ini telah lama ada tetapi di baru di pakai sekarang saat terjadi ledakan data. Contoh illustrasinya sebuah jeruk kita potong kita ambil sarinya dan manfaatkan sarinya, dan data science berperan seperti jeruk itu kita mencari sesuatu yang bermanfaat.
-Contoh pengaplikasian dari data klinis seperti mengecek tekanan darah, kolestrol, dll.
-Dan dari sinilah data science di perlukan untuk memilih sebuah model yang membantu kita membuat keputusan menjadi 2 kelompok, mana yang diabetes dan mana yang tidak diabetes.
-Data geometrics adalah salah 1 contoh pengapkasiannya data science seperti unlock face, sidik jari dan lain – lain di smartphone, jadi di situ ada data ada model ada nilai yang bisa mengenali seseorang.
+    Dari penjelasan di atas science adalah mencari sesautu value â€“ value atau nilai nilai tersebut yang bermanfaat.
+    Konsep Data science ini adalah metode lama tapi baru, maksudnya adalah metode ini telah lama ada tetapi di baru di pakai 
+    sekarang saat terjadi ledakan data. Contoh illustrasinya sebuah jeruk kita potong kita ambil sarinya dan manfaatkan sarinya, 
+    dan data science berperan seperti jeruk itu kita mencari sesuatu yang bermanfaat.
+    Contoh pengaplikasian dari data klinis seperti mengecek tekanan darah, kolestrol, dll.
+    Dan dari sinilah data science di perlukan untuk memilih sebuah model yang membantu kita membuat keputusan menjadi 2 kelompok,
+    mana yang diabetes dan mana yang tidak diabetes.
+    Data geometrics adalah salah 1 contoh pengapkasiannya data science seperti unlock face, 
+    sidik jari dan lain â€“ lain di smartphone, jadi di situ ada data ada model ada nilai yang bisa mengenali seseorang.
 Contoh aplikasi:
-1.	Kita bisa memilah berita hoax dan bukan
-2.	Data jaringan kita bisa mengidentifikasi bahaya hacking
+    1.	Kita bisa memilah berita hoax dan bukan
+    2.	Data jaringan kita bisa mengidentifikasi bahaya hacking
 
 d.	Metode untuk mencari sebuah value
-1.	Machine learning
-2.	Deep learning
+    1.	Machine learning
+    2.	Deep learning
+***
 
+##Materi Seminar II,
+###Waktu		      : 10/12/2019 : 09.00
+###Tempat		      : Rektorat UMMU
+###Durasi		      : 11.15 â€“ 12.54 Menit, Sesi Ke 2
+###Nama Pemateri	: Yudi Prayudi, M.Kom
+###Judul Materi	  : Overview Forensik Digital
+###Isi Materi	    : Ilmu Forensik, Forensik Digital, Kebutuhan SDM Forensik Digital, Pendidikan Forensik Digital, Penelitian Forensik Digital, Prospek Forensik Digital di Indonesia, Sharing Knowladge, Asosiasi Forensik Digital Indonesia (AFDI)
 
-Materi Seminar II,
-Waktu		: 10/12/2019 : 09.00
-Tempat		: Rektorat UMMU
-Durasi		: 11.15 – 12.54 Menit, Sesi Ke 2
-Nama Pemateri	: Yudi Prayudi, M.Kom
-Judul Materi	: Overview Forensik Digital
-Isi Materi	: Ilmu Forensik, Forensik Digital, Kebutuhan SDM Forensik Digital, Pendidikan Forensik Digital, Penelitian Forensik Digital, Prospek Forensik Digital di Indonesia, Sharing Knowladge, Asosiasi Forensik Digital Indonesia (AFDI)
-
-1.	Bring Your Own Device adalah salah 1 gaya hidup, dengan gaya hidup ini kita bisa bekerja di manapun tanpa memerlukan smartphone, laptop, dan nootbook kita.
+1.	Bring Your Own Device adalah salah 1 gaya hidup, dengan gaya hidup ini kita bisa bekerja di manapun tanpa memerlukan smartphone,
+    laptop, dan nootbook kita.
 2.	Our digital day adalah salah satu aktifitas yang selalu tidak lepas dari dunai maya
-3.	Semesta digital, menurut pa yudi kita sekarang berada di era cyber space karena aktifitas kita selalu di social media, jadi apa yang kita lakukan di dunia digital semua berdampak kembali kepada kita mau itu negative atau positif.
-4.	Identitas digital apa yang kita lakukan di dunia digital semua meniggalkan identitas kita di internet. Ada salah 1 situs yang bisa kita lihat yaitu ihave.com adalah salah 1 situs yang mengecek sebuah email yang pernah mengunjungi situs yang pernah di curi datanya. Dan di ihave.com kita bisa mengecek seberapa amannya password kita.
-5.	Jejak digital, setiap kali kita berinteraksi dengan dunia digital pasti kita akan meniggalkan jejak d dunia digital dan dari jejak ini orang bisa melakukan kejahatan, jadi jika kita membuat hal yang negative pasti akan meninggalkan jejak dan bisa terlacak.
-6.	Resiko, apa resikonya? Semakin banyak kita berinteraksi dengan dunia digital semakin beresiko, misalnya data – data pribadi kita di manfaatkan oleh orang – orang jahat dan masi banyak resiko resiko lainnya.
+3.	Semesta digital, menurut pa yudi kita sekarang berada di era cyber space karena aktifitas kita selalu di social media, 
+    jadi apa yang kita lakukan di dunia digital semua berdampak kembali kepada kita mau itu negative atau positif.
+4.	Identitas digital apa yang kita lakukan di dunia digital semua meniggalkan identitas kita di internet. 
+    Ada salah 1 situs yang bisa kita lihat yaitu ihave.com adalah salah 1 situs yang mengecek sebuah email yang pernah mengunjungi 
+    situs yang pernah di curi datanya. Dan di ihave.com kita bisa mengecek seberapa amannya password kita.
+5.	Jejak digital, setiap kali kita berinteraksi dengan dunia digital pasti kita akan meniggalkan jejak d dunia digital dan dari jejak
+    ini orang bisa melakukan kejahatan, jadi jika kita membuat hal yang negative pasti akan meninggalkan jejak dan bisa terlacak.
+6.	Resiko, apa resikonya? Semakin banyak kita berinteraksi dengan dunia digital semakin beresiko, misalnya data â€“ data pribadi kita 
+    di manfaatkan oleh orang â€“ orang jahat dan masi banyak resiko resiko lainnya.
+***
 
-
-
-Materi Seminar III,
-Waktu		: 10/12/2019 : 09.00
-Tempat		: Rektorat UMMU
-Durasi		: 9.30 – 10.15 Menit, Sesi Ke – 3
-Nama Pemateri	: Abdul Haris Muhammad, S.T., M.Kom
-Judul Materi	: Digital Lifestyle
-Isi Materi	: Digital Life, Digital Identity (identitas digital), The Impact Of Digital LifeStyle, Data Sciace And Digital Forensik Challenges
-
+##Materi Seminar III,
+###Waktu      		: 10/12/2019 : 09.00
+###Tempat		      : Rektorat UMMU
+###Durasi		      : 9.30 â€“ 10.15 Menit, Sesi Ke â€“ 3
+###Nama Pemateri	: Abdul Haris Muhammad, S.T., M.Kom
+###Judul Materi	  : Digital Lifestyle
+###Isi Materi	    : Digital Life, Digital Identity (identitas digital), The Impact Of Digital LifeStyle, Data Sciace And Digital Forensik Challenges
+***
 
 a.	Digital Life
-Digital Life adalah gaya hidup menggunakan digital. Menurut pa haris manusia sekarang memliki 2 kehidupan, kenapa? Karena sekarang adalah eranya internet. Menurut data survey pada bulan januari 2019 total manusia yang terkoneksi dengan internet adalah 150 juta dan pengguna mobile device adalah 142 juta pengguna.
+    Digital Life adalah gaya hidup menggunakan digital. Menurut pa haris manusia sekarang memliki 2 kehidupan, kenapa? Karena sekarang 
+    adalah eranya internet. Menurut data survey pada bulan januari 2019 total manusia yang terkoneksi dengan internet adalah 150 juta dan pengguna mobile device adalah 142 juta pengguna.
 
 b.	Digital Identity (identitas digital)
-Menurut pa haris digital identity ada hubungannya dengan keluarga, kenapa? Karena informasi-informasi kita di update di internet, karena kita terlalu memberi informasi yang bersifat personal dan sangat berbahaya jika di explore sama seseorang yang berniat jahat.
+    Menurut pa haris digital identity ada hubungannya dengan keluarga, kenapa? Karena informasi-informasi kita di update di internet, 
+    karena kita terlalu memberi informasi yang bersifat personal dan sangat berbahaya jika di explore sama seseorang yang berniat jahat.
 
 c.	The Impact Of Digital Lifestyle
-NOMOPHOBIA (No Mobile Phone Phobia)
-Apa itu NOMOPHOBIA? Contoh: manusia jaman sekarang lebih panik saat HPnya ketinggalan di rumah dari pada tas atau barang-barang yang lain ketinggalan dan itulah adalah Digutal Lifestyle. Dan menurut survey 80% perempuan di Mobile Devicenya lebih banyak foto dan untuk laki-laki 85% video itu menurut survey dari pa haris. Dari data yang di ambil dari kominfo 800 situs yang tidak bias dipercaya, karena 95% mahasiswa itu hanya membaca dari judulnya saja dan tidak membaca isi nya
-
+    NOMOPHOBIA (No Mobile Phone Phobia)
+    Apa itu NOMOPHOBIA? Contoh: manusia jaman sekarang lebih panik saat HPnya ketinggalan di rumah dari pada tas atau barang-barang yang
+    lain ketinggalan dan itulah adalah Digutal Lifestyle. Dan menurut survey 80% perempuan di Mobile Devicenya lebih banyak foto dan 
+    untuk laki-laki 85% video itu menurut survey dari pa haris. Dari data yang di ambil dari kominfo 800 situs yang tidak bias dipercaya,
+    karena 95% mahasiswa itu hanya membaca dari judulnya saja dan tidak membaca isinya.
 
 d.	Data Science & Digital Forensik Challenges
-Kalau dulu data pribadi susah di ambil dengan orang karena hanya menggunakan flashdisk. 
+    Kalau dulu data pribadi susah di ambil dengan orang karena hanya menggunakan flashdisk. 
