81e1b5fe44dcc890906d74a8828a0d6e4f7c2acf
sridantris <55886889+sridantris@users.noreply.github.com>
2020-01-08 12:57:43 +0700

    UAS_SRI DANTRIS ANHAL

---
+++ pgame16103/uas2.py
@@ -0,0 +1,23 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+done = False
+warna_bg = (250,222, 250)
+pygame.display.set_caption("Garis UAS")
+clock = pygame.time.Clock()
+while not done:
+	for event in  pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+		if event.type == pygame.KEYDOWN and \
+				event.key == pygame.K_ESCAPE:
+			done = True
+	screen.fill(warna_bg)
+	pygame.draw.line(screen, (250, 0, 0), (100, 0), (100, 400))
+	pygame.draw.line(screen, (250, 0, 0), (200, 0), (200, 400))
+	pygame.draw.line(screen, (250, 0, 0), (300, 0), (300, 400))
+	pygame.draw.line(screen, (250, 0, 0), (400, 300), (0, 300))
+	pygame.draw.line(screen, (250, 0, 0), (400, 200), (0, 200))
+	pygame.draw.line(screen, (250, 0, 0), (400, 100), (0, 100))
+	pygame.display.update()
+	clock.tick(110)
